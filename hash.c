f824ee7b9b18ed4cf74735ae6007b6036315c57b
d412vv1n <mr@master-studios.net>
2012-12-20 23:47:12 +0100

    implemented and tested quadratic-probing

--- hash.c
+++ hash.c
@@ -7,6 +7,7 @@
 #include <sys/mman.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
 /* gcc specific */
 typedef unsigned int uint128_t __attribute__((__mode__(TI)));
@@ -16,73 +17,91 @@ typedef unsigned int uint128_t __attribute__((__mode__(TI)));
 #define HASHSIZE (1<<20)
 
 struct block {
-    char *addr;
-    size_t len;
-} __attribute__((packed));
+	char *addr;
+	size_t len;
+};
 
 struct block slurp(char *filename) {
-    int fd=open(filename,O_RDONLY);
-    char *s;
-    struct stat statbuf;
-    struct block r;
-    if (fd==-1)
-        err(1, "%s", filename);
-    if (fstat(fd, &statbuf)==-1)
-        err(1, "%s", filename);
-    /* allocates an extra 7 bytes to allow full-word access to the last byte */
-    s = mmap(NULL,statbuf.st_size+7, PROT_READ|PROT_WRITE,MAP_FILE|MAP_PRIVATE,fd, 0);
-    if (s==MAP_FAILED)
-        err(1, "%s", filename);
-    r.addr = s;
-    r.len = statbuf.st_size;
-    return r;
+	int fd=open(filename,O_RDONLY);
+	char *s;
+	struct stat statbuf;
+	struct block r;
+	if (fd==-1)
+    err(1, "%s", filename);
+	if (fstat(fd, &statbuf)==-1)
+    err(1, "%s", filename);
+	/* allocates an extra 7 bytes to allow full-word access to the last byte */
+	s = mmap(NULL,statbuf.st_size+7, PROT_READ|PROT_WRITE,MAP_FILE|MAP_PRIVATE,fd, 0);
+	if (s==MAP_FAILED)
+		err(1, "%s", filename);
+	r.addr = s;
+	r.len = statbuf.st_size;
+	return r;
 }
 
 struct hashnode {
-    struct hashnode *next; /* link in external chaining */
-    char *keyaddr;
-    size_t keylen;
-    int value;
-} __attribute__((packed));
+	char *keyaddr;
+	size_t keylen;
+	int value;
+} __attribute__((__packed__));
 
 struct hashnode *ht[HASHSIZE];
 
 unsigned long hash(char *addr, size_t len) {
-    /* assumptions: 1) unaligned accesses work 2) little-endian 3) 7 bytes
-       beyond last byte can be accessed */
-    uint128_t x = 0, w;
-    size_t i, shift;
-    for (i = 0; i + 7 < len; i += 8) {
-        w = *(unsigned long *)(addr+i);
-        x = (x + w)*hashmult;
-    }
-    if (i < len) {
-        shift = ((i - len) << 3) + 64;
-        /* printf("len=%d, shift=%d\n",len, shift);*/
-        w = (*(unsigned long *)(addr + i)) << shift;
-        x = (x + w)*hashmult;
-    }
-    return x+(x>>64);
+	/* assumptions: 1) unaligned accesses work 2) little-endian 3) 7 bytes
+	   beyond last byte can be accessed */
+	uint128_t x=0, w;
+	size_t i, shift;
+	for (i=0; i+7<len; i+=8) {
+		w = *(unsigned long *)(addr+i);
+		x = (x + w)*hashmult;
+	}
+	if (i<len) {
+		shift = (i+8-len) << 3;
+		/* printf("len=%d, shift=%d\n",len, shift);*/
+		w = (*(unsigned long *)(addr+i))<<shift;
+		x = (x + w)*hashmult;
+	}
+	return x+(x>>64);
 }
 
 void insert(char *keyaddr, size_t keylen, int value) {
-    struct hashnode **l = &ht[hash(keyaddr, keylen) & (HASHSIZE-1)];
-    struct hashnode *n = malloc(sizeof(struct hashnode));
-    n->next = *l;
-    n->keyaddr = keyaddr;
-    n->keylen = keylen;
-    n->value = value;
-    *l = n;
+	struct hashnode **l;
+	int startPosition = hash(keyaddr, keylen) & (HASHSIZE-1);
+    int position = startPosition;
+    int i = 0;
+    do {
+        l = &ht[position];
+        position = (startPosition + (int) pow(-1, i) + (i*i/2)) % HASHSIZE;
+        i++;
+    } while(*l != NULL && position != startPosition);
+
+	if (*l == NULL) {
+		struct hashnode *n = malloc(sizeof(struct hashnode));
+		n->keyaddr = keyaddr;
+		n->keylen = keylen;
+		n->value = value;
+		*l = n;
+	}
 }
 
 int lookup(char *keyaddr, size_t keylen) {
-    struct hashnode *l = ht[hash(keyaddr, keylen) & (HASHSIZE-1)];
-    while (l!=NULL) {
-        if (keylen == l->keylen && memcmp(keyaddr, l->keyaddr, keylen) == 0)
-            return l->value;
-        l = l->next;
-    }
-    return -1;
+	int startPosition = hash(keyaddr, keylen) & (HASHSIZE-1);
+    int position = startPosition;
+	struct hashnode *l;
+    int i = 0;
+    do {
+        l = ht[position];
+        if (l == NULL) {
+            break;
+        }
+        if (keylen == l->keylen && memcmp(keyaddr, l->keyaddr, keylen) == 0) {
+			return l->value;
+		}
+        position = (startPosition + (int) pow(-1, i) + (i*i/2)) % HASHSIZE;
+        i++;
+    } while(position != startPosition);
+	return -1;
 }
 
 int main(int argc, char *argv[]) {
@@ -122,7 +141,7 @@ int main(int argc, char *argv[]) {
         sum, sumsq, HASHSIZE, ((double)sumsq)*HASHSIZE/sum-sum);
     /* expected value for chisq is ~HASHSIZE */
 #endif    
-	
+
     cache = calloc(HASHSIZE, sizeof(int));
     for (i = 0; i < 10; i++) {
         for (p = input2.addr, endp = input2.addr + input2.len, cacheCounter = 0; p < endp; cacheCounter++) {
